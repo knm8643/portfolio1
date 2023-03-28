758afc7331d85a50df461d7a6d9edac77b0b7b29
이충재 <knm8643@nate.com>
2023-03-28 22:33:24 +0900

    1차 작업

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="intro-page">
+    <div class="intro-text">
+      <h1>
+  <span class="char" v-for="(char, index) in title" :key="index" :ref="`titleChars[${index}]`">
+    {{ char }}
+  </span>
+        <span class="cursor" ref="titleCursor"></span>
+      </h1>
+         <br>
+      <p>
+  <span class="char" v-for="(char, index) in subtitle" :key="index" :ref="`subtitleChars[${index}]`">
+    {{ char }}
+  </span>
+        <span class="cursor" ref="subtitleCursor"></span>
+      </p>
+    </div>
+    <div class="intro-star"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import appJs from '@/js/app.js'
+export default appJs;
 </script>
 
 <style lang="scss" src="@/assets/scss/default.scss">
