05ceb840ec5600eb71c5c88df345a11cd2122ac4
이충재 <knm8643@nate.com>
2023-03-28 14:35:53 +0900

    패키지명 변경 / eslint, babelc 기초설정 / 디폴트 css경로 생성

---
+++ portfolio/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" src="@/assets/scss/default.scss">
+</style>
