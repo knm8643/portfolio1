ec378dddd63f0ba1ec6a85e77753c3e63f792f6e
이충재 <knm8643@nate.com>
2023-04-04 17:43:51 +0900

    경로재수정

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'prodcution'?'portfolio1':'/'
+  publicPath: 'portfolio1'
   // css: {
   //   loaderOptions: {
   //     sass: {
