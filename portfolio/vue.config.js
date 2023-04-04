457b4b56c824ce3c57c2723791e847f1e18731d4
이충재 <knm8643@nate.com>
2023-04-04 22:04:50 +0900

    깃io테스트7

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -3,12 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath : '/portfolio1/',
   // indexPath: '../dist/index.html',
-  // outputDir : 'root'
-  // css: {
-  //   loaderOptions: {
-  //     sass: {
-  //       additionalData: `@import "@/assets/scss/default.scss";`
-  //     }
-  //   }
-  // }
+  outputDir : 'docs'
 })
