d7e70f4d25dc7c126c6c290956004adba32f3d39
이충재 <knm8643@nate.com>
2023-04-04 21:30:44 +0900

    깃io테스트3

--- portfolio/vue.config.js
+++ portfolio/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath : '/portfolio1',
-  outputDir : 'docs'
+  outputDir : 'root'
   // css: {
   //   loaderOptions: {
   //     sass: {
