073ef0cb0298c4f12c4f988620ebb2af5df4d568
이충재 <knm8643@nate.com>
2023-04-07 18:23:30 +0900

    회사마지막 커밋 하단바 생성

--- portfolio/src/components/MoCompo.vue
+++ portfolio/src/components/MoCompo.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="mobile-wrapper">
     <div class="mobile-head">
-
     </div>
     <div class="mobile-body">
       <div class="body-Time">
@@ -10,11 +9,8 @@
       </div>
     </div>
     <div class="mobile-footer">
-      <div class="potos">
-        
-      </div>
-      <div class="fire">
-
+      <div class="footer_line">
+        <p>여기를 터치해주세요</p>
       </div>
     </div>
   </div>
