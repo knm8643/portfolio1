b55269400bf1e5bd2f8fc4f02f97e599758d7ded
이충재 <knm8643@nate.com>
2023-04-11 16:21:42 +0900

    살짝좀 피곤하기도 css조정

--- portfolio/src/components/HalfClub.vue
+++ portfolio/src/components/HalfClub.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="halfclub_box">
-    <a class="btn_t_cate"><i class="i_t_cate"></i></a>
-    <h1>클론코딩재밌네</h1>
+  <div class="halfclub-box">
+    <div class="main-text">
+      <h1>Welcome to My Shop</h1>
+    </div>
+    <div class="main-banner">
+      <img src="https://via.placeholder.com/375x200.png?text=Main+Banner" alt="Main Banner"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "HalfClub"
-}
+import halfClub from "@/js/halfClub.js";
+export default halfClub;
 </script>
 
 <style lang="scss" src="@/assets/scss/halfClub.scss">
